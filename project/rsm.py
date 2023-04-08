3536cf07c0965e4905988d6d228c31be09e80ec0
Paulsams <pasha.samsonov234@gmail.com>
2023-04-09 02:21:30 +0300

    Add get bool decomposition from rsm

--- project/rsm.py
+++ project/rsm.py
@@ -1,7 +1,9 @@
-from typing import NamedTuple, Dict
+from typing import NamedTuple, Dict, Any
 from pyformlang.finite_automaton import EpsilonNFA
 from pyformlang.cfg import Variable
 import project.ecfg as ecfg_utils
+from finite_automatons_utils import get_states_and_matrix_from_nfa
+from scipy.sparse import dok_matrix
 
 
 class RSM(NamedTuple):
@@ -17,6 +19,18 @@ def rsm_from_ecfg(ecfg: ecfg_utils.ECFG) -> RSM:
         ecfg.start_symbol,
         {head: body.to_epsilon_nfa() for head, body in ecfg.productions.items()},
     )
+
+
+def create_boolean_decomposition_from_rsm(
+    rsm: RSM,
+) -> Dict[Variable, Dict[Any, dok_matrix]]:
+    """
+    Convert to boolean decomposition from rsm
+    """
+    decompositions = {}
+    for key, value in rsm.boxes.items():
+        decompositions[key] = get_states_and_matrix_from_nfa(value)[1]
+    return decompositions
 
 
 def minimize_rsm(rsm: RSM) -> RSM:
